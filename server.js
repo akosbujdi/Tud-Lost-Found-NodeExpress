c8bb2153ea8d5ecf57b2d9490cb7b663851a2091
akosbujdi <145585869+akosbujdi@users.noreply.github.com>
2024-12-05 22:33:10 +0000

    report-and-get-lost-item-api-calls

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+require('dotenv').config({ path: './.env.local' });
+const lostItemsController = require('./controllers/lostItemsController');
+const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.use(cors());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Route to handle report lost item (delegated to controller)
+app.post('/api/report_lost', lostItemsController.upload.single('image'), (req, res) => {
+    lostItemsController.addLostItem(req, res); // Call the controller to handle the logic
 });
 
+app.get('/api/lost_items', lostItemsController.getLostItems);
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
